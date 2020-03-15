530aac1f4615904df569714b2800f72d1a104772
Nicolai Larsen <nicolailarsen2100@gmail.com>
2020-03-15 22:41:50 +0100

    kan hente fil med mongo

--- src/main/java/Database/DataSourceMongoDB.java
+++ src/main/java/Database/DataSourceMongoDB.java
@@ -1,15 +1,13 @@
 package Database;
 
-import com.mongodb.DB;
-import com.mongodb.Mongo;
-import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
+import com.mongodb.*;
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
 
 import java.net.UnknownHostException;
 import java.sql.SQLException;
 import java.util.List;
+import java.util.Set;
 
 public class DataSourceMongoDB {
 
@@ -18,17 +16,46 @@ public class DataSourceMongoDB {
     //Der er ingen security
     public static void main(String[] args) {
         try {
-            // MongoClient mongoClient = new MongoClient("", 27017);
-            //String url = "mongodb://130.225.170.214:27017";
-            //MongoClient mongoClient = new MongoClient(url);
-
             MongoClient mongoClient = new MongoClient("130.225.170.204", 27027);
-           DB db1 = mongoClient.getDB("cphPlaygrounds");
-            System.out.println(db1.getName());;
-           boolean auth = db1.authenticate("admin", "admin123".toCharArray());
-            List<String> dbs = mongoClient.getDatabaseNames();
-            for(String db : dbs){
+            DB database = mongoClient.getDB("cphPlaygroundsDB");
+            System.out.println(database.getCollection("playgrounds").findOne());
+            //  System.out.println(database.getName());
+            //database.createCollection("customers", null);
+            //boolean auth = database.authenticate("admin", "admin123".toCharArray());
+
+          /*  database.createCollection("playgrounds", null);
+            DBCollection collection = database.getCollection("playgrounds");
+            BasicDBObject document = new BasicDBObject();
+            document.put("name", "Shubham");
+            document.put("company", "Baeldung");
+            collection.insert(document);
+            database.getCollectionNames().forEach(System.out::println);
+            BasicDBObject query = new BasicDBObject();
+            query.put("name", "Shubham");
+            BasicDBObject newDocument = new BasicDBObject();
+            newDocument.put("name", "John");
+            BasicDBObject updateObject = new BasicDBObject();
+            updateObject.put("$set", newDocument);
+            collection.update(query, updateObject);
+*/
+
+            for (String name : database.getCollectionNames()
+            ) {
+                System.out.println(name);
+            }
+
+           /* List<String> dbs = mongoClient.getDatabaseNames();
+            for (String db : dbs) {
                 System.out.println(db);
+            }
+*/
+            DBCollection collection = database.getCollection("playgrounds");
+
+            BasicDBObject searchQuery = new BasicDBObject();
+            searchQuery.put("name", "John");
+            DBCursor cursor = collection.find(searchQuery);
+            while (cursor.hasNext()) {
+                System.out.println(cursor.next());
             }
 
         } catch (UnknownHostException e) {
