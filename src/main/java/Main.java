6ded7a1f851b282ba9ad2b5f82f41b2f74cc9da7
Nicolai Larsen <nicolailarsen2100@gmail.com>
2020-03-19 15:04:10 +0100

    rettet fejl i mvn dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -52,6 +52,7 @@ public class Main {
                 ctx.result(GalgelegResource.makeGuess(ctx.pathParam("username"), ctx.pathParam("guess"))).contentType("json"));
         app.get("rest/Playground", ctx ->
                 ctx.json(PlaygroundResource.getPlaygoundsList()).contentType("json"));
+        app.post("rest/snap", ctx -> ctx.result("snap"));
     }
 
     private static void formular(Context ctx) {
