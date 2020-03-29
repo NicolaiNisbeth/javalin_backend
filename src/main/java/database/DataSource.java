e1e9d54b54d36cb4017405150ac69dfd4e1d3705
Nicolai Larsen <nicolailarsen2100@gmail.com>
2020-03-29 19:37:49 +0200

    rettet db forbindelse og ændret projektnavn

--- src/main/java/database/DataSource.java
+++ src/main/java/database/DataSource.java
@@ -3,13 +3,12 @@ package database;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
-import com.mongodb.MongoClientURI;
 
 public class DataSource {
+    private final static String HOST = "130.225.170.204";
+    private final static int PORT = 27027;
+    private final static String DATABASE_NAME = "cphPlaygroundsDB";
     private static DB database;
-    private static String host = "130.225.170.204";
-    private static int port = 27027;
-    private static String databaseName = "cphPlaygroundsDB";
     private static MongoClient mongoClient;
 
     private DataSource() {
@@ -19,35 +18,41 @@ public class DataSource {
 	DB db = mongoClient.getDB("database name");
 	boolean auth = db.authenticate("username", "password".toCharArray());*/
 
-    /*
     public static DB getDB() {
         if (database == null) {
-            mongoClient = new MongoClient(host, port);
-            database = mongoClient.getDB("cphPlaygroundsDB");
+            mongoClient = new MongoClient(HOST, PORT);
+            database = mongoClient.getDB(DATABASE_NAME);
         }
-
         return database;
     }
 
-     */
+    public static MongoClient getClient() {
+        if (mongoClient == null)
+            mongoClient = new MongoClient(HOST, PORT);
+        return mongoClient;
+
+
+    }
 
 
 
 
-  public static DB getDB(){
+/*  public static DB getDB(){
         if (database == null)
             database = getClient().getDB("test");
 
         return database;
-  }
+  }*/
 
-  public static MongoClient getClient(){
+ /* public static MongoClient getClient(){
       if (mongoClient == null)
           mongoClient = new MongoClient(new MongoClientURI("mongodb+srv://s175565:qwe123@todoapp-cn8eq.mongodb.net/test"));
 
       return mongoClient;
-
-
   }
 
+  */
+
 }
+
+
