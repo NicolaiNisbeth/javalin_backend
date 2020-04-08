1656fae07241093500f69641a3db0931d55313d1
Nobert1 <s185031@student.dtu.dk>
2020-04-08 10:03:46 +0200

    merge

--- src/main/java/javalin_resources/Util/Path.java
+++ src/main/java/javalin_resources/Util/Path.java
@@ -12,33 +12,33 @@ public class Path {
      */
     public static class Playground {
         // All playgrounds.
-        public static final String PLAYGROUND_ALL = "/playgrounds";
+        public static final String PLAYGROUND_ALL = "/rest/playgrounds";
         // A playground with the given name.
-        public static final String PLAYGROUND_ONE = "/playgrounds/:name";
+        public static final String PLAYGROUND_ONE = "/rest/playgrounds/:name";
         // All employees on a playground
-        public static final String PLAYGROUND_ONE_PEDAGOGUE_ALL = "/playgrounds/:name/pedagogues";
+        public static final String PLAYGROUND_ONE_PEDAGOGUE_ALL = "/rest/playgrounds/:name/pedagogues";
         // One employees on a playground
-        public static final String PLAYGROUND_ONE_PEDAGOGUE_ONE = "/playgrounds/:name/pedagogues/:username";
+        public static final String PLAYGROUND_ONE_PEDAGOGUE_ONE = "/rest/playgrounds/:name/pedagogues/:username";
         // All events on all playgrounds.
-        public static final String PLAYGROUND_ALL_EVENTS = "/playgrounds/events";
+        public static final String PLAYGROUND_ALL_EVENTS = "/rest/playgrounds/events";
         // All events on a playground
-        public static final String PLAYGROUNDS_ONE_EVENTS_ALL = "/playgrounds/:name/events";
+        public static final String PLAYGROUNDS_ONE_EVENTS_ALL = "/rest/playgrounds/:name/events";
         // One events on a playground
-        public static final String PLAYGROUNDS_ONE_EVENT_ONE = "/playgrounds/:name/events/:id";
+        public static final String PLAYGROUNDS_ONE_EVENT_ONE = "/rest/playgrounds/:name/events/:id";
         // One event paricipant
-        public static final String PLAYGROUNDS_ONE_EVENT_ONE_PARTICIPANTS_ALL = "/playgrounds/:name/events/:id/participants";
+        public static final String PLAYGROUNDS_ONE_EVENT_ONE_PARTICIPANTS_ALL = "/rest/playgrounds/:name/events/:id/participants";
         // One event paricipant
-        public static final String PLAYGROUNDS_ONE_EVENT_ONE_PARTICIPANT_ONE = "/playgrounds/:name/events/:id/participants/:username";
+        public static final String PLAYGROUNDS_ONE_EVENT_ONE_PARTICIPANT_ONE = "/rest/playgrounds/:name/events/:id/participants/:username";
         // All events on a given month on a given playground.
-        public static final String PLAYGROUND_ONE_EVENTS_MONTH = "/playground/:name/events/:month";
+        public static final String PLAYGROUND_ONE_EVENTS_MONTH = "/rest/playground/:name/events/:month";
         // Playground message
-        public static final String PLAYGROUND_ONE_MESSAGE_ONE = "/playgrounds/:name/messages/:id";
+        public static final String PLAYGROUND_ONE_MESSAGE_ONE = "/rest/playgrounds/:name/messages/:id";
         // Playground message all
-        public static final String PLAYGROUND_ONE_MESSAGE_ALL = "/playgrounds/:name/messages";
+        public static final String PLAYGROUND_ONE_MESSAGE_ALL = "/rest/playgrounds/:name/messages";
         // All playgrounds with a given zipcode.
-        public static final String PLAYGROUNDS_WITHZIPCODE = "/playground/:zipcode";
+        public static final String PLAYGROUNDS_WITHZIPCODE = "/rest/playground/:zipcode";
         // All playgrounds nearby.
-        public static final String PLAYGROUNDS_NEARBY = "/playground/:nearby";
+        public static final String PLAYGROUNDS_NEARBY = "/rest/playground/:nearby";
     }
 
     public static class Employee {
