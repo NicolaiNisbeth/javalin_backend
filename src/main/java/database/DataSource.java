0a2b1b07f8e717ff6c84869562f0f481f987a59a
Nicolai Larsen <nicolailarsen2100@gmail.com>
2020-04-20 16:07:19 +0200

    implementeret - authentication af database kører

--- src/main/java/database/DataSource.java
+++ src/main/java/database/DataSource.java
@@ -3,16 +3,27 @@ package database;
 
 import com.mongodb.DB;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoDatabase;
+
+import java.util.Arrays;
 
 public class DataSource {
    /* private final static String HOST = "130.225.170.204";
     private final static int PORT = 27027;*/
 
-   private final static String HOST = "18.185.121.182";
+    private final static String HOST = "18.185.121.182";
     private final static int PORT = 27017;
     private final static String DATABASE_NAME = "cphPlaygroundsDB";
     private static DB database;
     private static MongoClient mongoClient;
+
+    private final static String user = "myAdmin"; // the user name
+    private final static String adminDatabase = "admin"; // the name of the database in which the user is defined
+    private final static char[] password = ("njl_nykode").toCharArray(); // the password as a character array
+    // ...
+
 
     private DataSource() {
     }
@@ -23,7 +34,11 @@ public class DataSource {
 
     public static DB getDB() {
         if (database == null) {
-            mongoClient = new MongoClient(HOST, PORT);
+            // mongoClient = new MongoClient(HOST, PORT);
+            // database = mongoClient.getDB(DATABASE_NAME);
+            MongoCredential credential = MongoCredential.createCredential(user, adminDatabase, password);
+            MongoClient mongoClient = new MongoClient(new ServerAddress(HOST, PORT),
+                    Arrays.asList(credential));
             database = mongoClient.getDB(DATABASE_NAME);
         }
         return database;
